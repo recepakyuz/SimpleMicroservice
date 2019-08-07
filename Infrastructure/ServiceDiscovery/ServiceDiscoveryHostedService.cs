b5a842df2b3d346d49a875ad4442e076d33188cc
recepakyuz <recepakyuz@mail.com>
2019-08-07 20:34:12 +0300

    Add files via upload

---
+++ Infrastructure/ServiceDiscovery/ServiceDiscoveryHostedService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Consul;
+using Microsoft.Extensions.Hosting;
+
+namespace Infrastructure.ServiceDiscovery
+{
+    public class ServiceDiscoveryHostedService : IHostedService
+    {
+        private readonly IConsulClient _client;
+        private readonly ServiceConfig _config;
+        private string _registrationId;
+
+        public ServiceDiscoveryHostedService(IConsulClient client, ServiceConfig config)
+        {
+            _client = client;
+            _config = config;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _registrationId = Guid.NewGuid().ToString();
+            var name = Dns.GetHostName(); // get container id
+            var ip = Dns.GetHostEntry(name).AddressList.FirstOrDefault(x => x.AddressFamily == AddressFamily.InterNetwork);
+            var registration = new AgentServiceRegistration
+            {
+                ID = _registrationId,
+                Name = _config.ServiceName,
+                Address = ip.ToString(),
+                Port = 80
+            };
+
+            await _client.Agent.ServiceDeregister(registration.ID, cancellationToken);
+            await _client.Agent.ServiceRegister(registration, cancellationToken);
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await _client.Agent.ServiceDeregister(_registrationId, cancellationToken);
+        }
+    }
+}
---
+++ TestService/Startup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using Consul;
+using Infrastructure.ServiceDiscovery;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace TestService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ConfigureConsul(services);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            //var name = Dns.GetHostName(); // get container id
+            //var ip = Dns.GetHostEntry(name).AddressList.FirstOrDefault(x => x.AddressFamily == AddressFamily.InterNetwork);
+
+            //var registration = new AgentServiceRegistration
+            //{
+            //    Name = "testservice",
+            //    Port = 80,
+            //    Address = "http://"+ip.ToString()
+            //};
+
+            //using (var client = new ConsulClient(config => { config.Address =new Uri("http://localhost:8500"); }))
+            //{
+            //    client.Agent.ServiceRegister(registration);
+            //}
+        }
+        private void ConfigureConsul(IServiceCollection services)
+        {
+            var serviceConfig = Configuration.GetServiceConfig();
+
+            services.RegisterConsulServices(serviceConfig);
+        }
+    }
+}
